cdd558dd23965d55868859fa6c2f109c51b962ce
mica635mrlv <millaregina635@gmail.com>
2025-09-16 10:48:43 -0300

    projeto finalizado

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+async function pesquisarCep(cep) {
+    const url = `https://cdn.apicep.com/file/apicep/${cep}.json`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+async function preencherCampos({ target }) {
+    const infoCep = await pesquisarCep(target.value)
+    document.getElementById('endereco').value = infoCep.address
+    document.getElementById('bairro').value = infoCep.district
+    document.getElementById('cidade').value = infoCep.city
+    document.getElementById('estado').value = infoCep.state
+}
+
+document.getElementById('cep').addEventListener('focusout', preencherCampos)
